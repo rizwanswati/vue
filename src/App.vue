25a9c1a6922f403804e51147001c93bd1a530138
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2023-09-25 16:03:10 +0500

    v-if

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-
+  const ok = false
+  const notOkay = true
+  const msg = "if okay is true this message will be shown"
 </script>
 
 <template>
-   <h1>directives</h1>
+   <h1 v-if="ok">{{msg + ok}}</h1>
+   <h1 v-if="notOkay">{{msg + ok}}</h1>
 </template>
