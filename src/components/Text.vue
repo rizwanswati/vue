165e40725c5549613886a92e07f44c1c30446131
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2023-09-25 14:41:42 +0500

    text interpolation

---
+++ src/components/Text.vue
@@ -0,0 +1,18 @@
+<script setup>
+  defineProps({
+    msg:{
+      required:true,
+      type:String
+    }
+  })
+</script>
+
+<template>
+  <h2>First message {{msg}}</h2>
+</template>
+
+<style scoped>
+  h2{
+    background: green;
+  }
+</style>
