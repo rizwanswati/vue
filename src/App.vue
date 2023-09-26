a0b441d2bbff7c558f80ac55ae98b3f69d3eaaaa
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2023-09-26 15:53:29 +0500

    reactive state ref

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 
+import ReactiveState from "@/components/ReactiveState.vue";
 </script>
 
 <template>
-
+  <ReactiveState></ReactiveState>
 </template>
 
 <style scoped>
---
+++ src/components/ReactiveState.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from "vue";
+var count = ref(0);
+console.log(count)
+console.log(count.value)
+
+function increment() {
+  count.value++
+}
+
+
+</script>
+
+<template>
+  <div>Simple</div>
+  <button @click="count++">count</button>
+  <div>{{count}}</div>
+
+  <div>complex</div>
+  <button @click="increment">increment</button>
+  <div>{{count}}</div>
+</template>
+
+<style scoped>
+
+</style>
